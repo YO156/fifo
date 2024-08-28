c3516780e442728ae90844c360cf158a499ce200
DHARSHINI <dharshiniyoga698@gmail.com>
2024-08-28 12:37:04 +0530

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,12 @@
+def recur_fibo(n):
+    if n<=1:
+        return n
+    else:
+        print("recur_fibo(n-1)+recur_fibo(n-2)")
+    nterms
+    nterms<=0
+    print("pleas enter positive number")
+    if(nterms):
+        print("Fibonacci sequence")
+        for i in range(nterms):
+           def recur_fibo()
